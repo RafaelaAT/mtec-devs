0ae1bd4b29aed75602a980bc22977d78ff20ad2b
Rafaela e Julia <rafaelaalessandratorelli@gmail.com>
2023-10-23 08:45:50 -0300

    Criação de método de login

--- MtecDevs/Controllers/AccountController.cs
+++ MtecDevs/Controllers/AccountController.cs
@@ -1,14 +1,24 @@
+using System.Net.Mail;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MtecDevs.ViewModels;
 
 namespace MtecDevs.Controllers;
 
 public class AccountController : Controller
 {
     private readonly ILogger<AccountController> _logger;
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly UserManager<IdentityUser> _userManager;
 
-    public AccountController(ILogger<AccountController> logger)
+    public AccountController(
+        ILogger<AccountController> logger,
+        SignInManager<IdentityUser> signInManager,
+        UserManager<IdentityUser> userManager)
     {
         _logger = logger;
+        _signInManager = signInManager;
+        _userManager = userManager;
     }
 
     [HttpGet]
@@ -16,10 +26,59 @@ public class AccountController : Controller
     {
         return View();
     }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(LoginVM login)
+    {
+        if (ModelState.IsValid)
+        {
+            // Verifica o Login
+            string userName = login.Email;
+            // Verificando se o login é por email
+            if (IsValidEmail(login.Email))
+            {
+                var user = await _userManager.FindByEmailAsync(login.Email);
+                if (user != null)
+                    userName = user.UserName;
+            }
+            // Login é só por UserName
+            var result = await _signInManager.PasswordSignInAsync(
+              userName, login.Senha, login.Lembrar, lockoutOnFailure: true
+            );
+
+            if (result.Succeeded)
+            {
+                _logger.LogInformation($"Usuário {login.Email} acessou o sistema");
+                return LocalRedirect(login.UrlRetorno);
+            }
+            if (result.IsLockedOut)
+            {
+                _logger.LogWarning($"Usuário {login.Email} está bloqueado");
+                return RedirectToAction("Lockout");
+            }
+            ModelState.AddModelError(string.Empty, "Usuário e/ou Senha Inválidos!");
+        }
+        return View(login);
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View("Error!");
     }
+
+    private static bool IsValidEmail(string email)
+    {
+        try
+        {
+            MailAddress mail = new(email);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
 }
